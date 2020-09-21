13af90cf1023ca96dbb7ef7e987945678d549357
qaz2345199876 <70868516+qaz2345199876@users.noreply.github.com>
2020-09-21 08:01:50 +0800

    Create work2.py

---
+++ work2.py
@@ -0,0 +1,28 @@
+#string='I love you'
+#s1=string[:1]
+#s2=string[2:6]
+#s3=string[7:]
+#print(s1)
+#print(s2)
+#print(s3)
+#########################################################
+
+#string='I love you'
+#s4=string[::-1]
+#print(s4)
+########################################################
+
+#string='I love you'
+#s='so'
+#s1=s*100
+#b='much'
+#print(string,s1,b)
+########################################################
+
+#string='I love you'
+#import random as r
+#a=r.randint(1,100)
+#b='so'
+#c=b*a
+#d='much'
+#print(string,c,d)
