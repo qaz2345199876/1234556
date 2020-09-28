51ceb9b292c11c8501decf4e5326821a9ac41e4d
qaz2345199876 <70868516+qaz2345199876@users.noreply.github.com>
2020-09-28 08:28:02 +0800

    Add files via upload

---
+++ work3.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# score=['小徐',5,9,6,8,7,10,6]
+# s1=score[1:]
+# a=max(s1)
+# print(a)
+
+#################################
+# score=['小徐',5,9,6,8,7,10,6]
+# s1=score[1:]
+# a=min(s1)
+# print(a)
+
+################################
+# score=['小徐',5,9,6,8,7,10,6]
+# s1=score[1:]
+# s1.sort(reverse=True)
+# sorted(s1)
+# x,y,z,q,w,e,r=s1
+# print(x,y,z)
+
+################################
+# score=['小徐',5,9,6,8,7,10,6]
+# s1=score[1:]
+# s1.sort(reverse=True)
+# sorted(s1)
+# x,y,z,q,w,e,r=s1
+# print(w,e,r)
+################################
+# score=['小徐',5,9,6,8,7,10,6]
+# s1=score[1:]
+# s2=sum(s1)/len(s1)
+# print(s2)
+
+################################
+# for i in range(1,10):
+#     for j in range(1,10):
+#         print(str(i)+'X'+str(j)+'='+f'{str(i*j):>2}'+'\t',end='')
+
+#     print()
+
+################################
+# import random as r
+# w= r.randint(1,100)
+# a='I love you'
+# print(a,end=' ')
+# for  b  in  range(w):
+#         b='so'
+#         print(b,end=' ') 
