b450d53fffd6520c9f507f6bb873e9d0b5d28a63
qaz2345199876 <70868516+qaz2345199876@users.noreply.github.com>
2020-10-05 10:44:13 +0800

    Add files via upload

---
+++ work4.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 09:53:41 2020
+
+@author: cis-user
+"""
+
+# a='Simon Peter John'
+# b=a.split()
+# t='*'
+# for i in range (0,3):
+#   print(b[i][0],end='')
+#   print(t*(len(b[i])-1),end=' ')
+
+# #################################
+
+# a=['Company 1','Company 2','Company 3']
+# c=[ ]
+# for i in range(0,3):
+#     c.append(a[i].replace(' ','_'))
+# print(c,end=' ')
+
+#################################
+
+# a=[1,2,3,4,5,6]
+# b=[]
+# c='$'
+# for i in a:
+#     b.append(str(i)+c)
+# print(b)
+
+################################
+
+# a=['1$', '2$', '3$', '4$', '5$', '6$']
+# b=[]
+# for i in range(0,6):
+#     b.append(int(a[i].replace('$','')))
+# print(b)
+
+###############################
+
+# a=[1,2,3,4]
+# b=[5,6,7,8]
+# c=list(zip(a,b))
+# print(c)
+
+################################
+
+# s="I love you and you love him and who loves who"
+# a=s.split()
+
+# keys=set(a)
+# values=[0 for i in keys]
+# d={key:value for key,value in zip(keys,values)}
+# for i in a:
+#     d[i]+=1
+# print(d)
+#########################################      
