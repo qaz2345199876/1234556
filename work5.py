8ab26cd6aac00f54561244e3f42d03776c24daac
qaz2345199876 <70868516+qaz2345199876@users.noreply.github.com>
2020-10-12 08:34:20 +0800

    Add files via upload

---
+++ work5.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 09:53:41 2020
+
+@author: cis-user
+"""
+
+    
+# list11=[1,2,3,4,5]
+# list12=[6,5,4,3,2]
+# for i in range(0,5):
+#     if list11[i]>list12[i]:
+#              print(list11[i],end='')
+#     else:
+#              print(list12[i],end='')
+
+########################
+
+# l1=[1,2,3,4,5]
+# l2=[6,5,4,3,2]
+# l3=[False,False,True,False,True]
+# for i in range(0,5):
+#     if l3[i]==True:
+#       print(l1[i],end='')
+#     else:
+#       print(l2[i],end='')
+
